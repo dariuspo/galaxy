5ad471429f00b9293dd0ef1b85ad510c22689396
dariuskln <darius@kapanlagi.net>
2019-12-31 13:14:15 +0700

    added matrix rotation to x axis for the whole galaxy. this will cause planets from edge not go in liner line.

--- lib/main.dart
+++ lib/main.dart
@@ -33,9 +33,35 @@ class Galaxy extends StatefulWidget {
 class _GalaxyState extends State<Galaxy> with TickerProviderStateMixin {
   Animation<Offset> cornerPlanetAnimation;
   Animation<Offset> spiralPlanetAnimation;
+  Animation<double> rotationAnimation;
 
   AnimationController cornerPlanetController;
   AnimationController spiralPlanetController;
+  AnimationController rotationGalaxyController;
+
+  static Matrix4 _pmat(num pv) {
+    return new Matrix4(
+      1.0,
+      0.0,
+      0.0,
+      0.0,
+      //
+      0.0,
+      1.0,
+      0.0,
+      0.0,
+      //
+      0.0,
+      0.0,
+      1.0,
+      pv * 0.001,
+      //
+      0.0,
+      0.0,
+      0.0,
+      1.0,
+    );
+  }
 
   @override
   void initState() {
@@ -49,17 +75,18 @@ class _GalaxyState extends State<Galaxy> with TickerProviderStateMixin {
         vsync: this);
     spiralPlanetAnimation = Tween(
       begin: Offset(0.0, 0.0),
-      end: Offset(8.0, 8.0),
+      end: Offset(2.0, 2.0),
     ).animate(
       CurvedAnimation(
         parent: spiralPlanetController,
         curve: Curves.linear,
       ),
     );
+
     cornerPlanetController = AnimationController(
         lowerBound: 0.0,
         upperBound: 1.0,
-        duration: const Duration(milliseconds: 3000),
+        duration: const Duration(milliseconds: 6000),
         vsync: this);
     cornerPlanetAnimation = Tween(
       begin: Offset(1.0, 1.0),
@@ -70,9 +97,25 @@ class _GalaxyState extends State<Galaxy> with TickerProviderStateMixin {
         curve: Curves.easeInCubic,
       ),
     );
+
+    rotationGalaxyController = AnimationController(
+        lowerBound: 0.0,
+        upperBound: 1.0,
+        duration: const Duration(milliseconds: 6000),
+        vsync: this);
+    rotationAnimation = Tween(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: rotationGalaxyController,
+        curve: Interval(0.0, 1.0, curve: Curves.linear),
+      ),
+    );
+
+    rotationGalaxyController.repeat();
     spiralPlanetController.repeat();
     cornerPlanetController.repeat();
   }
+
+  Matrix4 perspective = _pmat(1.0);
 
   @override
   Widget build(BuildContext context) {
@@ -89,128 +132,149 @@ class _GalaxyState extends State<Galaxy> with TickerProviderStateMixin {
         width: double.infinity,
         height: double.infinity,
         child: Center(
-          child: Stack(
-            children: <Widget>[
-              MovementAnimationBuilder(
-                radius: 100,
-                animationMovement: spiralPlanetAnimation,
-                child: ShapeBuilder(
-                  color: Colors.grey,
-                  radius: 30,
-                  boxShape: BoxShape.circle,
-                ),
-              ),
-              MovementAnimationBuilder(
-                radius: 160,
-                animationMovement: spiralPlanetAnimation,
-                child: ShapeBuilder(
-                  color: Colors.lightBlue,
-                  radius: 20,
-                  boxShape: BoxShape.rectangle,
-                ),
-              ),
-              MovementAnimationBuilder(
-                radius: 220,
-                animationMovement: spiralPlanetAnimation,
-                child: ShapeBuilder(
-                  color: Colors.deepOrange,
-                  radius: 40,
-                  boxShape: BoxShape.circle,
-                ),
-              ),
-              MovementAnimationBuilder(
-                radius: 300,
-                animationMovement: spiralPlanetAnimation,
-                child: TriangleBuilder(radius: 20,color: Colors.redAccent,)
-              ),
-              MovementAnimationBuilder(
-                radius: 400,
-                animationMovement: spiralPlanetAnimation,
-                child: ShapeBuilder(
-                  color: Colors.pink,
-                  radius: 60,
-                  boxShape: BoxShape.circle,
-                ),
-              ),
-              AnimatedBuilder(
-                animation: cornerPlanetAnimation,
-                builder: (context, child) => Transform.translate(
-                  offset: Offset(
-                    (screenWidth / 2) * cornerPlanetAnimation.value.dx,
-                    (screenHeight / (-2) - 100) *
-                        cornerPlanetAnimation.value.dy,
+          child: AnimatedBuilder(
+            animation: rotationAnimation,
+            builder: (context, child) => Transform(
+              alignment: FractionalOffset.center,
+              transform: perspective.scaled(1.0, 1.0, 1.0)
+                ..rotateX(pi - (rotationAnimation.value * 360) * pi / 180)
+                ..rotateY(0.0)
+                ..rotateZ(0.0),
+              child:
+                  /*RotationTransition(
+                turns:rotationAnimation,
+                child: */
+                  Stack(
+                children: <Widget>[
+                  MovementAnimationBuilder(
+                    radius: 100,
+                    animationMovement: spiralPlanetAnimation,
+                    child: ShapeBuilder(
+                      color: Colors.grey,
+                      radius: 30,
+                      boxShape: BoxShape.circle,
+                    ),
                   ),
-                  child: TriangleBuilder(
-                    color: Colors.deepOrange,
-                    radius: 40,
+                  MovementAnimationBuilder(
+                    radius: 160,
+                    animationMovement: spiralPlanetAnimation,
+                    child: ShapeBuilder(
+                      color: Colors.lightBlue,
+                      radius: 20,
+                      boxShape: BoxShape.rectangle,
+                    ),
                   ),
-                ),
+                  MovementAnimationBuilder(
+                    radius: 220,
+                    animationMovement: spiralPlanetAnimation,
+                    child: ShapeBuilder(
+                      color: Colors.deepOrange,
+                      radius: 40,
+                      boxShape: BoxShape.circle,
+                    ),
+                  ),
+                  MovementAnimationBuilder(
+                      radius: 300,
+                      animationMovement: spiralPlanetAnimation,
+                      child: TriangleBuilder(
+                        radius: 20,
+                        color: Colors.redAccent,
+                      )),
+                  MovementAnimationBuilder(
+                    radius: 400,
+                    animationMovement: spiralPlanetAnimation,
+                    child: ShapeBuilder(
+                      color: Colors.pink,
+                      radius: 60,
+                      boxShape: BoxShape.circle,
+                    ),
+                  ),
+                  AnimatedBuilder(
+                    animation: cornerPlanetAnimation,
+                    builder: (context, child) => Transform.translate(
+                      offset: Offset(
+                        (screenWidth / 2) * cornerPlanetAnimation.value.dx,
+                        (screenHeight / (-2) - 100) *
+                            cornerPlanetAnimation.value.dy,
+                      ),
+                      child: TriangleBuilder(
+                        color: Colors.deepOrange,
+                        radius: 40,
+                      ),
+                    ),
+                  ),
+                  AnimatedBuilder(
+                    animation: cornerPlanetAnimation,
+                    builder: (context, child) => Transform.translate(
+                      offset: Offset(
+                        (screenWidth / (-2) - 100) *
+                            cornerPlanetAnimation.value.dx,
+                        (screenHeight / (-2)) * cornerPlanetAnimation.value.dy,
+                      ),
+                      child: ShapeBuilder(
+                        color: Colors.white,
+                        radius: 40 * cornerPlanetAnimation.value.dx,
+                        boxShape: BoxShape.circle,
+                      ),
+                    ),
+                  ),
+                  AnimatedBuilder(
+                    animation: cornerPlanetAnimation,
+                    builder: (context, child) => Transform.translate(
+                      offset: Offset(
+                        (screenWidth / 2 + 300) *
+                            cornerPlanetAnimation.value.dx,
+                        (screenHeight / 2) * cornerPlanetAnimation.value.dy,
+                      ),
+                      child: ShapeBuilder(
+                        color: Colors.cyanAccent,
+                        radius: 80 * cornerPlanetAnimation.value.dx,
+                        boxShape: BoxShape.rectangle,
+                      ),
+                    ),
+                  ),
+                  AnimatedBuilder(
+                    animation: cornerPlanetAnimation,
+                    builder: (context, child) => Transform.translate(
+                      offset: Offset(
+                        (screenWidth / (-2) - 200) *
+                            cornerPlanetAnimation.value.dx,
+                        (screenHeight / (-2)) * cornerPlanetAnimation.value.dy,
+                      ),
+                      child: ShapeBuilder(
+                        color: Colors.white,
+                        radius: 40 * cornerPlanetAnimation.value.dx,
+                        boxShape: BoxShape.rectangle,
+                      ),
+                    ),
+                  ),
+                  AnimatedBuilder(
+                    animation: cornerPlanetAnimation,
+                    builder: (context, child) => Transform.translate(
+                      offset: Offset(
+                        (screenWidth / (-2) - 200) *
+                            cornerPlanetAnimation.value.dx,
+                        (screenHeight / 2) * cornerPlanetAnimation.value.dy,
+                      ),
+                      child: ShapeBuilder(
+                        color: Colors.green,
+                        radius: 40 * cornerPlanetAnimation.value.dx,
+                        boxShape: BoxShape.circle,
+                      ),
+                    ),
+                  ),
+                  Transform.translate(
+                    offset: Offset(0.0, 0.0),
+                    child: ShapeBuilder(
+                      radius: 50,
+                      color: Colors.black,
+                      boxShape: BoxShape.circle,
+                    ),
+                  ),
+                ],
+                /*),*/
               ),
-              AnimatedBuilder(
-                animation: cornerPlanetAnimation,
-                builder: (context, child) => Transform.translate(
-                  offset: Offset(
-                    (screenWidth / (-2) - 100) * cornerPlanetAnimation.value.dx,
-                    (screenHeight / (-2)) * cornerPlanetAnimation.value.dy,
-                  ),
-                  child: ShapeBuilder(
-                    color: Colors.white,
-                    radius: 40 * cornerPlanetAnimation.value.dx,
-                    boxShape: BoxShape.circle,
-                  ),
-                ),
-              ),
-              AnimatedBuilder(
-                animation: cornerPlanetAnimation,
-                builder: (context, child) => Transform.translate(
-                  offset: Offset(
-                    (screenWidth / 2 + 100) * cornerPlanetAnimation.value.dx,
-                    (screenHeight / 2) * cornerPlanetAnimation.value.dy,
-                  ),
-                  child: ShapeBuilder(
-                    color: Colors.cyanAccent,
-                    radius: 80 * cornerPlanetAnimation.value.dx,
-                    boxShape: BoxShape.rectangle,
-                  ),
-                ),
-              ),
-              AnimatedBuilder(
-                animation: cornerPlanetAnimation,
-                builder: (context, child) => Transform.translate(
-                  offset: Offset(
-                    (screenWidth / (-2) - 100) * cornerPlanetAnimation.value.dx,
-                    (screenHeight / (-2)) * cornerPlanetAnimation.value.dy,
-                  ),
-                  child: ShapeBuilder(
-                    color: Colors.white,
-                    radius: 40 * cornerPlanetAnimation.value.dx,
-                    boxShape: BoxShape.rectangle,
-                  ),
-                ),
-              ),
-              AnimatedBuilder(
-                animation: cornerPlanetAnimation,
-                builder: (context, child) => Transform.translate(
-                  offset: Offset(
-                    (screenWidth / (-2) - 100) * cornerPlanetAnimation.value.dx,
-                    (screenHeight / 2) * cornerPlanetAnimation.value.dy,
-                  ),
-                  child: ShapeBuilder(
-                    color: Colors.green,
-                    radius: 40 * cornerPlanetAnimation.value.dx,
-                    boxShape: BoxShape.circle,
-                  ),
-                ),
-              ),
-              Transform.translate(
-                offset: Offset(0.0, 0.0),
-                child: ShapeBuilder(
-                  radius: 50,
-                  color: Colors.black,
-                  boxShape: BoxShape.circle,
-                ),
-              ),
-            ],
+            ),
           ),
         ),
       ),
--- lib/widget/movement-animation-builder.dart
+++ lib/widget/movement-animation-builder.dart
@@ -21,9 +21,9 @@ class _MovementAnimationBuilderState extends State<MovementAnimationBuilder> {
       animation: widget.animationMovement,
       builder: (context, child) => Transform.translate(
         offset: Offset(
-          widget.radius * cos(widget.animationMovement.value.dx * pi/(widget.radius/100)),
+          widget.radius * cos(widget.animationMovement.value.dx * pi),
           (widget.radius / 1.5) *
-              sin(widget.animationMovement.value.dy * pi/(widget.radius/100)),
+              sin(widget.animationMovement.value.dy * pi),
         ),
         child: widget.child,
       ),
